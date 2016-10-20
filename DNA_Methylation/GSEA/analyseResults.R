1c62979a54104723ff025bbc9d8e611518939f69
SCP-028 <jzhou0317@gmail.com>
2016-10-20 17:43:55 -0400

    Not finished yet.

--- DNA_Methylation/GSEA/analyseResults.R
+++ DNA_Methylation/GSEA/analyseResults.R
@@ -1,15 +1,15 @@
 subsetBy <- function(df, project = NULL, type = c("normal", "tumor"),
-                      coef = NULL, annotation = NULL, cutoff = NULL,
-                      score = c("ES", "R2")){
+                      annotation = NULL, score = c("ES", "R2"),
+                      coef = NULL){
 #   Return a subset of the original data frame
     if (!is.null(project)){
         project <- paste0("(", project, ")", collapse = "|")
         tag.project <- grep(ignore.case = T, project, colnames(df))
         df <- df[ ,c(1, tag.project)]
     }
-    type <- paste0("(", type, ")", collapse = "|")
-    score <- paste0("(", score, ")", collapse = "|")
+    type <- paste0("(", type, ")", collapse = "|")  # Choose type
     df <- df[ ,c(1, grep(ignore.case = T, type, colnames(df)))]
+    score <- paste0("(", score, ")", collapse = "|")  # Choose score type
     df <- df[ ,c(1, grep(ignore.case = F, score, colnames(df)))]
     if (!is.null(coef)){
         coef <- paste0(coef, collapse = "")
@@ -21,10 +21,6 @@ subsetBy <- function(df, project = NULL, type = c("normal", "tumor"),
         annotation <- paste0("(", annotation, ")", collapse = "|")
         tag.annotation <- grep(ignore.case = T, annotation, df$annotation)
         df <- df[tag.annotation, ]
-    }
-    if (!is.null(cutoff)){
-#   cutoff = abs(tumor - normal)
-        
     }
     return(df)
 }
@@ -47,4 +43,21 @@ getGenes <- function(keyword, gsc = NULL){
                             symbol = sub("(.*)\\|.*", "\\1", pathways))
     rownames(pathways) <- 1:nrow(pathways)
     try(return(pathways))
+}
+
+compareSameProj <- function(df, project, cutoff = 0.5, type){
+#   Compare differences between normal and cancer samples
+#   cutoff = abs(tumor - normal)
+#   type: cancer - Cancer better than normal
+#         normal - Normal better than cancer
+    df <- subsetBy(df, project = project)
+    tag.ES <- grep("ES", colnames(df))
+    x.ES <- grep(paste(type, "ES", sep = ".*"), colnames(df))
+    y.ES <- tag.ES[!(tag.ES %in% x.ES)]  # Always x > y
+    tag.R2 <- grep("R2", colnames(df))
+    x.R2 <- grep(paste(type, "R2", sep = ".*"), colnames(df))
+    y.R2 <- tag.R2[!(tag.R2 %in% x.R2)]
+    for(i in seq_along(tag.ES.normal)){
+        
+    }
 }
