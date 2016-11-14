04f685f762b9b234a9b36dff796b6479b22fdee5
SCP-028 <jzhou0317@gmail.com>
2016-11-14 14:11:27 +0800

    Initial commit from Windows!

--- Python/dragonLootPlus.py
+++ Python/dragonLootPlus.py
@@ -1,12 +1,16 @@
 from collections import Counter
-inventory = Counter({'gold':42,
-					'rope':1})
+inventory = Counter({'gold': 42,
+                    'rope': 1})
+
+
 def addToInventory(inventory, addedItems):
-	inventory.update(addedItems)
-addedItems = ['gold','gold','dagger','ruby']
-addToInventory(inventory,addedItems)
+    inventory.update(addedItems)
+
+
+addedItems = ['gold', 'gold', 'dagger', 'ruby']
+addToInventory(inventory, addedItems)
 totalNum = 0
 for k, v in inventory.items():
-	print(k + ': ' + str(v))
-	totalNum += int(v)
+    print(k + ': ' + str(v))
+    totalNum += int(v)
 print('Total items: ' + str(totalNum))
