8851425c9aa293d32fc14be7d2cc64fc62564717
SCP-028 <jzhou0317@gmail.com>
2017-09-01 14:13:06 -0400

    Regular update.

---
+++ histone_acetylation/nnls.R
@@ -0,0 +1,78 @@
+## Use the non-negative linear least squares method to calculate
+# Initial settings
+setwd("C:/Users/jzhou/Desktop/")
+set.seed(1005)
+source("https://raw.githubusercontent.com/ggrothendieck/gsubfn/master/R/list.R")
+library(dplyr)
+
+# Prepare to load and save data
+file_name <- list.files("./expression/", pattern="RData$")
+projects <- sub("(.*)\\.up.*$", "\\1", file_name)
+results <- vector("list", length = (length(projects) * 2))
+names(results) <- as.vector(outer(projects, c("_normal", "_tumor"), paste0))
+
+
+gen.regex <- function(gene_name, ...) {
+#   Collapse string array into a regular expression
+    gene_name <- c(gene_name, unlist(list(...), recursive=T))
+    return(paste0(sub("(.*)", "(^\\1\\\\|)", gene_name), collapse="|"))
+}
+
+
+retrieve.gene <- function(df, regex) {
+#   Retrieve genes using a regular expression from large data frames.
+    df <- as.data.frame(df[grep(regex, rownames(df)), , drop=F])
+    rownames(df) <- sub("(.*)\\|.*$", "\\1", rownames(df))
+    return(df)
+}
+
+
+build.nnls <- function(gen.df, con.df) {
+#   Turn input data frames into suitable ones for package nnls.
+#   gen.df leaves one gene on the LHS, and the others are taken the opposite
+#   number and moved to the RHS.
+    library(nnls)
+    if (nrow(gen.df >= 1)) {
+        y <- as.numeric(as.character(gen.df[1, ]))
+        x <- rbind(con.df, -1 * gen.df[-1, , drop=F])
+        df <- nnls(t(x), y)
+        df$gen.genes <- rownames(gen.df)
+        df$con.genes <- rownames(con.df)
+        names(df$x) <- rownames(x)
+        return(df)
+    }
+    else {
+        return(NA)
+    }
+}
+
+
+# Constants
+generate_name <- gen.regex("ACLY","ACSS2")
+consume_name <- gen.regex("ACC1", "ACC2", "HMGR", "ACOT1", "ACOT7", "ACOT11", "ACOT12", "ACOT13", "CREBBP", "EP300", "KAT2B", "KAT5", "NCOA1", "NCOA3")
+
+
+for (i in seq_along(projects)) {
+    load(paste0("./expression/", file_name[i]))
+    # Get genes that are related to Ac-CoA
+    gen.normal <- retrieve.gene(datan, generate_name)
+    gen.tumor <- retrieve.gene(datat, generate_name)
+    con.normal <- retrieve.gene(datan, consume_name)
+    con.tumor <- retrieve.gene(datat, consume_name)
+    # Build non-negative least square linear models
+    results[[2*i-1]] <- build.nnls(gen.normal, con.normal)
+    results[[2*i]] <- build.nnls(gen.tumor, con.tumor)
+}
+
+# Merge coefficients into one data frame
+coef.df <- data.frame(symbol=character())
+for (i in seq_along(results)) {
+    df <- results[[i]]
+    x <- as.data.frame(df$x)
+    colnames(x) <- paste0(names(results)[i], "_", df$gen.genes[!df$gen.genes %in% names(df$x)])
+    x$symbol <- rownames(x)
+    coef.df <- full_join(coef.df, x, by = "symbol")
+}
+
+save(results, coef.df, file="./nnls_models.RData")
+
--- histone_acetylation/t_test.R
+++ histone_acetylation/t_test.R
@@ -1,69 +1,57 @@
 ## Calculates the expression of UCPs in normal vs. cancer tissues, and draw density plots.
 setwd("C:/Users/jzhou/Desktop/")
+set.seed(1005)
 source("https://raw.githubusercontent.com/ggrothendieck/gsubfn/master/R/list.R")
 library(ggplot2)
-library(reshape)
 
 file_name <- list.files("./expression/", pattern="RData$")
 projects <- sub("(.*)\\.up.*$", "\\1", file_name)
 
-df <- matrix(nrow = length(projects), ncol = 5,
-                dimnames = list(projects, c("exp.normal", "exp.tumor", "fold.change", "p.value", "sig")))
+result <- data.frame()
+p.value.cutoff <- 0.05
 
-result <- vector("list", 5)  # 改成gene_name长度
-
-
-
-
-gene_name <- names(result) <- c("ACSS1", "ACSS2")  # Add other genes here.
-
-
-
-
-result <- lapply(result, function(x) return(df))
-result <- result[order(names(result))]
-
-
-prepare.plot <- function(df, tissue="normal") {
-    x <- as.data.frame(rowSums(df) / ncol(df))
-    x$tissue <- tissue
-    x$gene <- rownames(x)
-    df <- as.data.frame(df)
-    df$gene <- rownames(df)
-    df$tissue <- tissue
-    df <- melt(df)
-    return(list(df, x))
-}
+gene_name <- c("MCT1","MCT4","SMCT1","SMCT4","HDAC\\d","HDAC10","HDAC11","SIRT\\d","CREBBP","EP300","KAT2B","KAT5","NCOA1","NCOA3","ACSS1", "ACSS2","ACSS1","HMGR","PDHA1","PDHB","CPT1A","CPT1B","CPT2","CS","ACOT12","ACOT2","ACOT9","ACOT10","ACOT13","ACOT1","ACOT4","ACOT7", "ACOT8","ACOT9" ,"ACOT11","ACOT13")
+regex_gene_name <- paste0(sub("(.*)", "(^\\1\\\\|)", gene_name), collapse="|")
 
 
 for (i in seq_along(projects)) {
     load(paste0("./expression/", file_name[i]))
-    ucp.normal <- datan[grep("(^ACSS1\\|)|(^ACSS2\\|)", rownames(datan)), , drop=F]
-    ucp.tumor <- datat[grep("(^ACSS1\\|)|(^ACSS2\\|)", rownames(datat)), , drop=F]
+    ucp.normal <- datan[grep(regex_gene_name, rownames(datan)), , drop=F]
+    ucp.tumor <- datat[grep(regex_gene_name, rownames(datat)), , drop=F]
     rownames(ucp.normal) <- sub("(.*)\\|.*$", "\\1", rownames(ucp.normal))
     rownames(ucp.tumor) <- sub("(.*)\\|.*$", "\\1", rownames(ucp.tumor))
-    ucp.normal <- ucp.normal[order(rownames(ucp.normal)), , drop=F]
-    ucp.tumor <- ucp.tumor[order(rownames(ucp.tumor)), , drop=F]
+    temp <- matrix(nrow = nrow(ucp.normal), ncol = 6)
+    colnames(temp) <- c("symbol", "project", "normal_exp", "tumor_exp", "p_value", "regulation")
     for (j in seq(nrow(ucp.normal))) {  # t-test of normal and tumor tissues
         x <- t.test(ucp.normal[j, ], ucp.tumor[j, ])
-        result[[rownames(ucp.normal)[j]]][i, 1:3] <- c(x$estimate[1], x$estimate[2], (x$estimate[2] / x$estimate[1]))
-        if(x$estimate[2] > x$estimate[1]) {
-            result[[rownames(ucp.normal)[j]]][i,4] <- x$p.value
-            result[[rownames(ucp.normal)[j]]][i,5] <- ifelse(x$p.value <= 0.05, "Y", "N")
-        }
+        temp[j,1:5] <- c(rownames(ucp.normal)[j], projects[i], x$estimate[1], x$estimate[2], x$p.value)
+        temp[j,6] <- ifelse(x$p.value <= p.value.cutoff, (x$estimate[2] / x$estimate[1]), 1)
     }
-    list[ucp.normal, mean.normal] <- prepare.plot(ucp.normal, "normal")
-    list[ucp.tumor, mean.tumor] <- prepare.plot(ucp.tumor, "tumor")
-    df <- rbind(ucp.normal, ucp.tumor)
-    mean.df <- rbind(mean.normal, mean.tumor)
-    colnames(mean.df) <- c("mean.value", "tissue", "gene")
-    for (j in seq_along(gene_name)) {  # Plot for each gene in certain cancer types
-        ggplot(df[df$gene == gene_name[j], ], aes(x=value, fill=tissue))+
-        geom_density(alpha=.3)+
-        geom_vline(data=mean.df[mean.df$gene == gene_name[j], ], aes(xintercept=mean.value, color=tissue), linetype="dashed", size=1)+
-        ggtitle(paste0(projects[i], "_", gene_name[j]))
-        ggsave(filename=paste0("./",projects[i], "_", gene_name[j], ".png"), width = 12, height = 9, units = "in")
-    }
+    result <- rbind(result, as.data.frame(temp))
 }
 
+save(result, file="./t_test.RData")
+df <- result[ ,c("symbol", "project", "regulation")]
+df <- reshape(df, idvar = "symbol", timevar = "project", direction = "wide")
+colnames(df) <- sub("regulation.", "", colnames(df))
+ord <- hclust(dist(df[ ,-1], method = "euclidean"), method = "ward.D2")$order
+df.m <- melt(df, id.vars = "symbol", variable_name = "project")
+colnames(df.m) <- c("symbol", "project", "log2_fold_change")
+df.m$symbol <- factor(df.m$symbol, levels = df$symbol[ord])
+df.m$log2_fold_change <- log2(as.numeric(as.character(df.m$log2_fold_change)))
+
+
+base_size <- 9
+ggplot(df.m, aes(symbol, project, fill=log2_fold_change)) +
+    geom_tile() +
+    # scale_fill_brewer(type = "div", palette = 5) +  # Red & Blue
+    scale_fill_gradient2(low="#1fddff", high="#ff4b1f", na.value = "gray95") +
+    theme_grey(base_size = base_size) +
+    labs(x = "", y = "") +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0)) +
+    theme(axis.text.x = element_text(size = base_size * 0.8, angle = 330, hjust = 0)) +
+    ggtitle("T-test for histone acetylation genes")
+
+ggsave(filename = "./t_test_acetylation.png", width = 12, height = 9, units = "in")
 save(result, file="./t_test.RData")
