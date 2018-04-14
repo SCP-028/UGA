6a9afae17579669afc4733dee46330ded8a50e57
SCP-028 <SCP-028@users.noreply.github.com>
2018-04-13 23:48:34 -0400

    Working code for environment pH estimate, but results are not realistic.

--- protein_pka/pH_estimate.R
+++ protein_pka/pH_estimate.R
@@ -7,9 +7,13 @@
 rm(list=ls())
 
 setwd("C:/Users/yz73026/Desktop/protein_pka/KAUST/result")
-require(dplyr)
+require(tidyverse)
 require(glue)
 require(ggpubr)
+
+PROTEIN_PERCENT <- 0.165
+WATER_MOLECULAR_WEIGHT <- 18.01528
+WATER_ION_PRODUCT <- 1.55e-7
 
 df <- data.table::fread("./pka_dataframe.csv",
                         stringsAsFactors = F, data.table = F, header = T, sep = ",")
@@ -17,13 +21,14 @@ acidic <- c("ASP", "GLU")
 basic <- c("ARG", "LYS", "HIS")
 
 eq_acid <- function(pKa) {
-  return(sqrt(10 ^ pKa))
+  return((10 ^ -pKa))
 }
 
 eq_base <- function(pKb) {
-  return(-(10 ^ pKb))
+  return(-(10 ^ -pKb))
 }
 
+# calculate Ka or Kb value for each residue
 df <- df %>%
   filter(Residue %in% c(acidic, basic)) %>%
   mutate(pH = ifelse(Residue %in% acidic,
@@ -32,61 +37,74 @@ df <- df %>%
   group_by(Protein) %>%
   summarise(protein_pH = sum(pH))
 
-# Map to Ensembl gene ID
+# map to Ensembl gene ID
 # ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz
+# we have to use uniprot ID as an intermediate
 ensemblAnnot <- data.table::fread("C:/Users/yz73026/Desktop/annotation/HUMAN_9606_idmapping.dat",
                            sep = "\t", stringsAsFactors = F, data.table = F, header = F)
 colnames(ensemblAnnot) <- c("uniprot", "database", "ID")
-uniprot_pdb <- ensemblAnnot %>%
+df <- ensemblAnnot %>%
   filter(database == "PDB") %>%
   rename(Protein = ID) %>%
-  select(Protein, uniprot)
-df <- left_join(df, uniprot_pdb, by = "Protein")
+  select(Protein, uniprot) %>%
+  right_join(df, by="Protein")
+
 uniprot_ensembl <- ensemblAnnot %>%
   filter(database == "Ensembl") %>%
   rename(ensembl = ID) %>%
   select(ensembl, uniprot)
 df <- inner_join(df, uniprot_ensembl, by = "uniprot")
-df <- df %>%
-  select(ensembl, protein_pH) %>%
+# save unique uniprot IDs
+write.csv(unique(df$uniprot), file = "unique_uniprot.csv", row.names = F, quote = F)
+# ph: data.frame w/ columns: ensembl & overall_pH
+ph <- df %>%
+  select(Protein, ensembl, protein_pH) %>%
     group_by(ensembl) %>%
-    summarise(overall_pH=sum(protein_pH))
-# write.csv(df, file="./ensembl_charge_pH7.0.csv", quote=F, row.names=F)
+    summarise(overall_pH=mean(protein_pH))
 
-# Expression data and cancer stage information
+# https://web.expasy.org/cgi-bin/compute_pi/pi_tool to retrieve protein molecular weight
+mw <- read.table("protein_mass.tsv")
+colnames(mw) <- c("pdb", "uniprot", "pI", "weight")
+# mw: data.frame w/ columns uniprot & weight
+mw <- mw %>%
+  group_by(uniprot) %>%
+  summarise(mw=mean(weight)) %>%
+  left_join(uniprot_ensembl, by="uniprot")
+
+
+estimate_ph <- function(exp, ph, mw) {
+  #' Estimate environment pH given gene expression information.
+  #'
+  #' Require tidyverse to work.
+  #'
+  #' @param exp Data.frame with columns ensembl & expression.
+  #' @param ph Data.frame with columns ensembl & overall_pH.
+  #' @param mw Data.frame with columns ensembl & mw (molecular weight).
+  #'
+  #' @return A pH value (might be way out of range).
+  rownames(exp) <- sub("\\..*$", "", rownames(exp))
+  exp <- rowMeans(exp[rownames(exp) %in% ph$ensembl, ])
+  exp <- data.frame(ensembl=names(exp), expression=exp)
+  mw <- mw %>%
+    inner_join(exp, by="ensembl") %>%
+    mutate(product=expression * mw) %>%
+    select(ensembl, product)
+  water <- sum(mw$product) * (1 - PROTEIN_PERCENT) / (PROTEIN_PERCENT * WATER_MOLECULAR_WEIGHT)
+  ph <- ph %>%
+    inner_join(exp, by="ensembl") %>%
+    mutate(product= expression * overall_pH) %>%
+    select(ensembl, product)
+  ph <- 7 - log10(1 + sum(ph$product) / (WATER_ION_PRODUCT * water))
+  return (ph)
+}
+
+
+# load expression data and cancer stage information
 filenames <- list.files("C:/Users/yz73026/Desktop/expression_FPKM/", pattern=".RData$", full.names = T)
 projects <- sub(".*/([A-Z-]+)\\.RData", "\\1", filenames)
 for (i in seq_along(projects)) {
   load(filenames[i])  # datan, datat, annot
-  rownames(datan) <- rownames(datat) <- sub("\\..*$", "", rownames(datan))
-  datan <- rowMeans(datan[rownames(datan) %in% df$ensembl, ])
-  datat <- rowMeans(datat[rownames(datat) %in% df$ensembl, ])
-  datan <- data.frame(ensembl=names(datan), expression=datan)
-  datat <- data.frame(ensembl=names(datat), expression=datat)
-  dfn <- inner_join(datan, df, by="ensembl")
-  dft <- inner_join(datat, df, by="ensembl")
-  dfn <- dfn %>%
-    mutate(
-      charge_expression=ifelse(Net_charge >= 0,
-                               log2(overall_pH * expression + 0.1),
-                               -log2(-overall_pH * expression + 0.1)
-                               ),
-      sample="control",
-      project=projects[i]
-    )
-  dft <- dft %>%
-    mutate(
-      charge_expression=ifelse(Net_charge >= 0,
-                               log2(Net_charge * expression + 0.1),
-                               -log2(-Net_charge * expression + 0.1)
-                               ),
-      sample="tumor",
-      project=projects[i]
-    )
+  phn <- estimate_ph(exp=datan, ph=ph, mw=mw)
+  pht <- estimate_ph(exp=datat, ph=ph, mw=mw)
   result <- rbind.data.frame(dfn, dft)
-  p <- ggdensity(result, x = "charge_expression", add="mean", fill="sample", color="sample")
-  p <- ggpar(p, ggtheme=theme_minimal(), palette = c("#00AFBB", "#E7B800"),
-             title=projects[i], xlab="log2 (Net charge * expression + 0.1)", ylab="Density")
-  ggsave(filename=glue("{projects[i]}.tiff"),
-           plot=p, device="tiff", width=10, height=6, units="in", dpi=200)
 }
