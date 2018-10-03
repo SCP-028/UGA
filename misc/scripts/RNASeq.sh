42b074e57f312a2c6025349ba89710a3f6185a2f
SCP-028 <SCP-028@users.noreply.github.com>
2018-10-03 16:50:06 -0400

    More generic version.

--- misc/scripts/RNASeq.sh
+++ misc/scripts/RNASeq.sh
@@ -1,37 +1,50 @@
-#!/bin/sh
-# GSE78011
-declare -a SRX=("SRX1589783" "SRX1589784" "SRX1589785" "SRX1589789" "SRX1589790")
-# MCF7 - 1, 2, 3    |  MDA MB-231 - 1, 2
+#!/bin/bash
+PROJECT_NAME="GSE75168" # single-ended
+declare -a SRX=("SRX1438068" "SRX1438069" "SRX1438070" "SRX1438074" "SRX1438075" "SRX1438076")
+# MCF10A - 1, 2, 3    |  MDA MB-231 - 1, 2, 3
 ROOTDIR=$(pwd)
 SOFTWAREDIR="$ROOTDIR/software"
 FASTQDIR="$ROOTDIR/fastq"
-mkdir -p "$FASTQDIR/qc" "$SOFTWAREDIR"
+USE_CACHE=true  # if false, remove all existing data / software
 
-cd "$SOFTWAREDIR"
+###############################################################################
+#                Shouldn't need to modify below this line                     #
+###############################################################################
+mkdir -p "$FASTQDIR/qc" "$SOFTWAREDIR" && cd "$SOFTWAREDIR"
 
 # sratoolkit: fastq file download
-if [ ! -d sratoolkit* ]; then
+if [ ! -d sratoolkit* ] || [ ! USE_CACHE ]; then
+    rm -rf ./sratoolkit*
     wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
     tar -xzvf sratoolkit.current-ubuntu64.tar.gz && rm sratoolkit.current-ubuntu64.tar.gz
 fi
 export PATH="$(find $SOFTWAREDIR -type d -name 'sratoolkit*')/bin:$PATH"
 
 # FastQC: pre-alignment QC
-if [ ! -d FastQC* ]; then
+if [ ! -d FastQC* ]|| [ ! USE_CACHE ]; then
+    rm -rf ./FastQC
     wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip
     unzip fastqc_v0.11.7.zip && rm fastqc_v0.11.7.zip
+    chmod +x FastQC/fastqc
 fi
 export PATH="$SOFTWAREDIR/FastQC:$PATH"
 
+if [ ! -d Trimmomatic* ] || [ ! USE_CACHE]; then
+    wget "http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.38.zip"
+    unzip Trimmomatic-0.38.zip && rm Trimmomatic-0.38.zip
+fi
+
 # HISAT2: alignment
-if [ ! -d hisat2* ]; then
+if [ ! -d hisat2* ]|| [ ! USE_CACHE ]; then
+    rm -rf ./hisat2*
     wget "ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/downloads/hisat2-2.1.0-Linux_x86_64.zip"
     unzip hisat2-2.1.0-Linux_x86_64.zip && rm hisat2-2.1.0-Linux_x86_64.zip
 fi
 export PATH="$(find $SOFTWAREDIR -type d -name 'hisat2*' | tail -n 1):$PATH"
 
 # Samtools: view BAM files
-if [ ! -d samtools* ]; then
+if [ ! -d samtools* ]|| [ ! USE_CACHE ]; then
+    rm -rf ./samtools*
     wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
     bzip2 -d samtools-1.9.tar.bz2 && tar -xvf samtools-1.9.tar && rm samtools-1.9.tar
     cd samtools-1.9
@@ -46,14 +59,17 @@ fi
 export PATH="$(find $SOFTWAREDIR -type d -name 'samtools*')/pkg/bin:$PATH"
 
 # StringTie: transcript assembler
-if [ ! -d stringtie* ]; then
+if [ ! -d stringtie* ]|| [ ! USE_CACHE ]; then
+    rm -rf ./stringtie*
     wget "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.4d.Linux_x86_64.tar.gz"
     tar -xzvf stringtie-1.3.4d.Linux_x86_64.tar.gz && rm stringtie-1.3.4d.Linux_x86_64.tar.gz
 fi
 export PATH="$(find $SOFTWAREDIR -type d -name 'stringtie*'):$PATH"
 
 # download genome index files
-if [ ! -d "$ROOTDIR/grch38" ];then
+cd $ROOTDIR
+if [ ! -d "$ROOTDIR/grch38" ]|| [ ! USE_CACHE ];then
+    rm -rf "$ROOTDIR/grch38"
     wget "ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/data/grch38.tar.gz"
     tar xzvf grch38.tar.gz && rm grch38.tar.gz
     bash grch38/make_grch38.sh
@@ -64,22 +80,55 @@ fi
 
 #####################################################
 
-# download fastq files
-cd "$FASTQDIR"
+mkdir -p "$FASTQDIR/$PROJECT_NAME" && cd "$FASTQDIR/$PROJECT_NAME"
+if [ ! USE_CACHE ];then
+    rm -rf $HOME/ncbi/public/sra
+fi
 for f in "$SRX[@]"
 do
-    prefetch "$f"
-    vdb-dump -f fastq "$HOME/ncbi/public/sra/$f.sra" > "$f.fastq"
-    # pre-alignment quality control reports
-    # mkdir -p "$FASTQDIR/qc/$f"
-    # fastqc "$f.fastq" -o "./qc/$f" -t 4
-    ########################################################
-    # Check quality control reports before moving forward! #
-    ########################################################
+    # download fastq files
+    if [ ! -f "$f.fastq" ];then
+        prefetch "$f" && vdb-dump -f fastq "$HOME/ncbi/public/sra/$f.sra" > "$f.fastq"
+    fi
+done
+
+# pre-alignment quality control reports
+if [ ! -d "$FASTQDIR/$PROJECT_NAME/qc/before_trim" ];then
+    mkdir -p "$FASTQDIR/$PROJECT_NAME/qc/before_trim"
+    fastqc $(printf "%s.fastq " "${SRX[@]}") -o "$FASTQDIR/$PROJECT_NAME/qc/before_trim" -t ${#SRX[@]}
+fi
+
+for f in "$SRX[@]"
+do
+    # trim adapters
+    if [ ! -f "$FASTQDIR/$PROJECT_NAME/$f.trimmed.fq" ];then
+        java -jar "$SOFTWAREDIR/Trimmomatic-0.38/trimmomatic-0.38.jar" SE -threads 30 -phred33 "$f.fastq" "$f.trimmed.fq" \
+            ILLUMINACLIP:"$SOFTWAREDIR/Trimmomatic-0.38/adapters/TruSeq3-SE.fa":2:30:10 \
+            LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 && rm "$f.fastq"
+    fi
+done
+
+if [ ! -d "$FASTQDIR/$PROJECT_NAME/qc/after_trim" ];then
+    mkdir -p "$FASTQDIR/$PROJECT_NAME/qc/after_trim"
+    fastqc $(printf "%s.trimmed.fq " "${SRX[@]}") -o "$FASTQDIR/$PROJECT_NAME/qc/after_trim/" -t ${#SRX[@]}
+fi
+
+# Check quality control reports before moving forward!
+###########################################################################
+# Per base sequence quality should be mostly in the green range           #
+# Per sequence GC content should be within the 30%-70% range              #
+# Sequence length distribution could help determine MINLEN in trimmomatic #
+# Adapter content should be around 0 after trimming                       #
+###########################################################################
+
+for f in "$SRX[@]"
+do
     # align reads
-    hisat2 --dta -p 30 -x "$ROOTDIR/grch38/genome" -U "$FASTQDIR/$f.fastq" -S "$f.sam" && rm "$f.fastq"
+    hisat2 --dta -p 30 \
+        -x "$ROOTDIR/grch38/genome" \
+        -U "$FASTQDIR/$PROJECT_NAME/$f.trimmed.fq" \
+        -S "$f.sam" && rm "$f.trimmed.fq"
     # convert SAM to BAM
-    # samtools view -@ 20 -b -o "$f.sam" "$f.bam"
     samtools sort -@ 30 -o "$f.bam" "$f.sam" && rm "$f.sam"
     # transcript assembly
     stringtie -p 30 -G "$ROOTDIR/grch38/Homo_sapiens.GRCh38.94.gff3" -o "$f.gtf" -l "$f" "$f.bam"
@@ -90,8 +139,10 @@ stringtie --merge -p 30 -G "$ROOTDIR/grch38/Homo_sapiens.GRCh38.94.gff3" -o stri
 
 for f in "$SRX[@]"
 do
-    mkdir -p "$FASTQDIR/ballgown/$f"
-    stringtie -e -B -p 30 -G stringtie_merged.gtf -o "$FASTQDIR/ballgown/$f/$f.gtf" "$f.bam"
+    rm "$f.gtf"
+    mkdir -p "$FASTQDIR/$PROJECT_NAME/ballgown/$f"
+    stringtie -e -B -p 30 -G stringtie_merged.gtf -o "$FASTQDIR/$PROJECT_NAME/ballgown/$f/$f.gtf" "$f.bam"
 done
+cd $ROOTDIR
 
 # Use R package ballgown for differential expression analysis
--- misc/scripts/ballgown.R
+++ misc/scripts/ballgown.R
@@ -1,34 +1,42 @@
 # GSE78011
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("ballgown", "genefilter", "devtools", "tidyverse"))
+devtools::install_github('alyssafrazee/RSkittleBrewer')
 library(ballgown)
 library(RSkittleBrewer)
 library(genefilter)
 library(dplyr)
-library(devtools)
 
+setwd("~/data/ballgown")
 # phenotype data for the samples
-SRX <- c("SRX1589783" "SRX1589784" "SRX1589785" "SRX1589789" "SRX1589790")
-cell_type <- c(rep("MCF-7", 3), rep("MDA-MB-231", 2))
+project <- "GSE75168"
+SRX <- c("SRX1438068", "SRX1438069", "SRX1438070", "SRX1438074", "SRX1438075", "SRX1438076")
+cell_type <- c(rep("MCF10A", 3), rep("MDA-MB-231", 3))
 pheno_data <- data.frame(SRX, cell_type)
 
 # read expression data calculated by StringTie
-bg_chrX <- ballgown(dataDir = "ballgown", samplePattern = "SRX", pData=pheno_data)
+bg <- ballgown(dataDir = project, samplePattern = "SRX", pData=pheno_data)
 
 # filter out low abundance genes
-bg_chrX_filt <- subset(bg_chrX,"rowVars(texpr(bg_chrX)) >1",genomesubset=TRUE)
+bg_filt <- subset(bg,"rowVars(texpr(bg)) >1",genomesubset=TRUE)
 
 # identify differentially expressed transcripts and genes
-results_transcripts <- stattest(bg_chrX_filt, feature="transcript",covariate="cell_type", getFC=TRUE, meas="FPKM")
-results_transcripts =data.frame(geneNames=ballgown::geneNames(bg_chrX_filt), geneIDs=ballgown::geneIDs(bg_chrX_filt), results_transcripts)
-results_genes <- stattest(bg_chrX_filt, feature="gene",covariate="cell_type", getFC=TRUE, meas="FPKM")
+results_transcripts <- stattest(bg_filt, feature="transcript",covariate="cell_type", getFC=TRUE, meas="FPKM")
+results_transcripts <- data.frame(geneNames=ballgown::geneNames(bg_filt), geneIDs=ballgown::geneIDs(bg_filt), results_transcripts)
+
+results_genes <- stattest(bg_filt, feature="gene",covariate="cell_type", getFC=TRUE, meas="FPKM")
+indices <- match(results_genes$id, texpr(bg, 'all')$gene_id)
+gene_names_for_result <- texpr(bg, 'all')$gene_name[indices]
+results_genes <- data.frame(geneNames=gene_names_for_result, results_genes)
 
 results_transcripts <- arrange(results_transcripts,pval)
 results_genes <- arrange(results_genes,pval)
 
-write.csv(results_transcripts, "TNBC_transcript.csv", row.names=FALSE)
-write.csv(results_genes, "TNBC_gene.csv", row.names=FALSE)
+write.csv(results_transcripts, paste0(project, "/TNBC_transcript.csv"), row.names=FALSE)
+write.csv(results_genes, paste0(project, "/TNBC_gene.csv"), row.names=FALSE)
 
-subset(results_transcripts,results_transcripts$qval<0.05)
-subset(results_genes,results_genes$qval<0.05)
+results_transcripts <- subset(results_transcripts,results_transcripts$qval<0.05)
+results_genes <- subset(results_genes,results_genes$qval<0.05)
 
-write.csv(results_transcripts, "TNBC_transcript_q_0.05.csv", row.names=FALSE)
-write.csv(results_genes, "TNBC_gene_q_0.05.csv", row.names=FALSE)
+write.csv(results_transcripts, paste0(project, "/TNBC_transcript_q_0.05.csv"), row.names=FALSE)
+write.csv(results_genes, paste0(project, "/TNBC_gene_q_0.05.csv"), row.names=FALSE)
